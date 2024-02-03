e5423aed69b7ca7739504a51dc815af109d0a5bf
Rafael Santos <126109568+Rafaeelsantos@users.noreply.github.com>
2024-02-03 11:55:17 -0300

    Update desafio1.dart

--- Curso de Dart/Desafios/desafio1.dart
+++ Curso de Dart/Desafios/desafio1.dart
@@ -1,25 +1,9 @@
-int encontrarMaior(List<int> lista) {
-  if (lista.isEmpty) {
-  }
+void main () {
 
-  int maiorNumero = lista[0];  // Assume que o primeiro elemento é o maior inicialmente
+    List<int> lista = [0, 9, 3, 6, 4];
 
-  for (int numero in lista) {
-    if (numero > maiorNumero) {
-      maiorNumero = numero;
-    }
-  }
+    int maiorValor = lista.reduce((valorAtual, proximoValor) =>
+    valorAtual > proximoValor ? valorAtual : proximoValor);
+print('O maior valor na lista é: $maiorValor');
 
-  return maiorNumero;
-}
-
-void main() {
-  List<int> listaDeNumeros = [5, 2, 8, 1, 9, 3, 6];
-  int resultado = encontrarMaior(listaDeNumeros);
-
-  if (resultado != null) {
-    print('O maior número na lista é: $resultado');
-  } else {
-    print('A lista está vazia.');
-  }
 }
